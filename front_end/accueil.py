f6a19b9473b57be5d5969291698bedab05257319
Clement Lesage <clement.lesage4@gmail.com>
2024-03-18 17:15:32 +0100

    frontend_test3

--- front_end/accueil.py
+++ front_end/accueil.py
@@ -27,7 +27,17 @@ for col, name in zip(num_cols, num_col_names):
     user_input[name] = col.number_input(name, min_value=0, value=0, step=1, key=name)
 
 # Bouton pour envoyer les données
-if st.button('Envoyer les données'):
+if st.button('Prédiction  du rating'):
+    # Remplacez 'your_api_endpoint' par l'URL de votre API
+    api_endpoint = 'your_api_endpoint'
+
+    # Envoi des données à l'API
+    response = requests.post(api_endpoint, json=user_input)
+
+    # Affichage de la réponse de l'API
+    st.write(response.text)
+
+if st.button('Prédiction du nombre de joueur à la sortie'):
     # Remplacez 'your_api_endpoint' par l'URL de votre API
     api_endpoint = 'your_api_endpoint'
 
