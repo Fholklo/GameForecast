93a4f3b723487449219aa7fd84ca447110b84629
Clement Lesage <clement.lesage4@gmail.com>
2024-03-18 18:28:01 +0100

    frontend_test12

--- front_end/accueil.py
+++ front_end/accueil.py
@@ -3,8 +3,25 @@ import requests
 
 # Définition des noms des colonnes et leurs étiquettes correspondantes
 text_col_names = [("App_ID", "ID de l'application"), ("Release_Date", "Date de sortie"), ("Supported_Languages", "Langues supportées"), ("Support_URL", "URL du support"), ("Developers", "Développeurs"), ("Publishers", "Éditeurs"), ("Categories", "Catégories")]
-genre_options = [...]
-category_options = [...]
+genre_options = ['Action', 'Casual', 'Indie', 'RPG', 'Simulation', 'Adventure',
+                 'Strategy', 'Design & Illustration', 'Video Production',
+                 'Early Access', 'Massively Multiplayer', 'Free to Play', 'Sports',
+                 'Animation & Modeling', 'Utilities', 'Game Development',
+                 'Photo Editing', 'Software Training', 'Nudity', 'Violent',
+                 'Racing', 'Gore', 'Sexual Content', 'Audio Production',
+                 'Web Publishing', 'Movie', 'Education', 'Accounting']
+category_options = ['Single-player', 'Steam Cloud', 'Family Sharing', 'Steam Achievements',
+                    'Partial Controller Support', 'Full controller support', 'Multi-player',
+                    'Steam Trading Cards', 'Steam Workshop', 'Co-op', 'Online Co-op',
+                    'Steam Leaderboards', 'PvP', 'Online PvP', 'Remote Play on Phone',
+                    'Remote Play on Tablet', 'Remote Play on TV', 'In-App Purchases',
+                    'Tracked Controller Support', 'VR Only', 'MMO', 'Cross-Platform Multiplayer',
+                    'Stats', 'Includes level editor', 'Shared/Split Screen',
+                    'Remote Play Together', 'No', 'VR Supported', 'Captions available',
+                    'VR Support', 'Shared/Split Screen PvP', 'Shared/Split Screen Co-op',
+                    'Valve Anti-Cheat enabled', 'LAN Co-op', 'Steam Turn Notifications',
+                    'HDR available', 'LAN PvP', 'Commentary available', 'Includes Source SDK',
+                    'SteamVR Collectibles', 'Mods', 'Mods (require HL2)']
 bool_col_names = ["Windows", "Mac", "Linux"]
 num_col_names = [("Achievements", "Nombre de succès disponibles"), ("Price", "Prix en euros")]
 
